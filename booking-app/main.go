55af2107ebbbdb3fc9573ba8ec2c50355dd865e9
Tenison <tenisonoseiowusu@gmail.com>
2022-03-09 15:52:37 +0000

    updated booking-app with inputs for booking

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,6 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("hello\n")
-	fmt.Print("How are you")
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int8
+
+	fmt.Print("Please enter your first name : ")
+	fmt.Scan(&firstName)
+
+	fmt.Print("Please enter your last name : ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("Email Address : ")
+	fmt.Scan(&email)
+
+	fmt.Print("Please enter number of ticket : ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("%v %v, you purhased %d tickets, confirmation will be sent to your email at %s", firstName, lastName,  userTickets, email)
+
 }
