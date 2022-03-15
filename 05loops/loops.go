41fb7f5db169c4b30764b73efa00b63f99e763d6
Tenison <tenisonoseiowusu@gmail.com>
2022-03-15 21:50:57 +0000

    loop with arrays and slices update

--- 05loops/loops.go
+++ 05loops/loops.go
@@ -1,15 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //loops in go
 func main() {
+	//loop basic form
 	max := 5
-
 	for i := 0; i < max; i++ {
 		fmt.Println("Hello", i)
 	}
 
+	//range iterates over elements in a variety of data structures
+	//Example : Add the series of 10 number
+	var total int8
+	numberList := []int8{12, 3, 45, 2, 5, 65, 12, 43, 54, 10}
+
+	for _, number := range numberList {
+		total += number
+	}
+	fmt.Println("Total of 10 numbers is : ", total)
+
+	//forever loop
 	for {
 		max++
 		if max == 10 {
