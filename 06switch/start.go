00250ed85880c1b74939dc01ad558ffe791831ed
Tenison <tenisonoseiowusu@gmail.com>
2022-04-04 00:26:41 +0100

    simple switch statment updated

--- 06switch/start.go
+++ 06switch/start.go
@@ -18,6 +18,19 @@ func main() {
 		fmt.Println("three")
 	}
 
+	//Simple Switch
+	//STAGE 2
+	gg := 2
+	fmt.Print("Write ", i, " as ")
+	switch gg {
+	case 1:
+		fmt.Println("one")
+	case 2, 3:
+		fmt.Println("two or three")
+	case 4:
+		fmt.Println("four")
+	}
+
 	//Having multiple cases :time.Saturday, time.Sunday
 	switch time.Now().Weekday() {
 	case time.Saturday, time.Sunday:
