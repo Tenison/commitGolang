16c26ebe53272dbe2f995e1ecca2f6aa4c2fb6ef
Tenison <tenisonoseiowusu@gmail.com>
2022-06-11 20:35:33 +0000

    working with strings in GO

--- 02variables/varExp/arithmeticOp.go
+++ 02variables/varExp/arithmeticOp.go
@@ -20,5 +20,28 @@ func SimpleOperations() {
 }
 
 func ShiftOperations() {
+	// bit shifting
+	a := 8  // = 2^3 or 2 pow 3
+	b := 16 // = 2^4 or 2 pow 4
+
+	fmt.Println(a << 3) // (2^3) * (2^3) = (2^6)// Ans = 64
+	fmt.Println(a >> 2) // (2^3) / (2^2) = (2^1)// Ans = 2
+
+	fmt.Println(b << 3) // (2^4) * (2^3) = (2^7)// Ans = 128
+	fmt.Println(b >> 2) // (2^4) / (2^2) = (2^2)// Ans = 4
+}
+
+func BitOperations() {
+	a := 10 //1010
+	b := 3  //0011
+
+	//these operations are carried out using binary value
+	c := a & b //AND // 2 //0010
+	d := a | b //OR // 11 //1011
+	e := a ^ b //XOR // 9 //1001 //means one of the numbers has (bit set or not) but not both.
+
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
 
 }
--- 02variables/varExp/varTypes.go
+++ 02variables/varExp/varTypes.go
@@ -37,19 +37,34 @@ func DeclareFloat() {
 	var a float32 = 32
 	var b float64
 
+	//default foalt64
+	c := 64.7
+
 	fmt.Println(a)
 	fmt.Println(b)
+	fmt.Println(c)
 
 }
 
 func DeclareComplex() {
-	var a complex64 = 1 + 3i
-	var b complex128
+	var a complex64 = 1 + 3i //float32 + float32
+	var b complex128         ////float64 + float64
 	c := complex(2, 3)
 
 	fmt.Printf("%v %v %T", real(a), imag(a), a)
 	fmt.Println(b)
 	fmt.Println(c)
+}
+
+func DeclareString() {
+	//String is a series of charaters
+	//Each charater is represented with a byte or sometimes bytes depending on the charater.
+	//Ecah byte or bytes has a numerical value(Integer Number) or code point value.
+	//code point value  -> numerical representation of a charater in a string Eg:  d => 100
+	//GO uses UTF8 encoding to transform/encode these charaters into binary or sequence of binary with is then stored in memory
+	//Because Computers understand data in o's and 1's.
+	//A UFT8 charater charater range between 1 and 4 bytes. This is because there are a lot of charaters and symbols for different languages.
+
 }
 
 func TypeCasting() {
