c63ee499fa4f09e1a84a71cb9a853f859024da41
Tenison <tenisonoseiowusu@gmail.com>
2022-06-21 23:06:07 +0000

    go multidimensional array basics

--- 02variables/collections/arrays.go
+++ 02variables/collections/arrays.go
@@ -8,6 +8,7 @@ var packageArray [5]float32
 //array declaration
 func DeclareArray (){
 	var newArray [6]int //Declare basic array of size 6
+	var newArrayRedudent [4]float32 = [4]float32{34.3, 56.6, 40.3, 12}
 	newArray1 := [6]string{"Osei", "Yaw", "Alex", "Nash", "Michael", "Frank"} //Array Literal
 	newArray2 := [...]string{"Osei", "Yaw", "Michael", "Frank"} ///Initize array to whatever value size that is passed in
 
@@ -15,6 +16,7 @@ func DeclareArray (){
 	newArray[1] = 2
 	newArray[2] = 3 
 	
+	fmt.Printf("%v", newArrayRedudent)
 	fmt.Printf("%v", newArray[0])
 	fmt.Printf("%v", newArray1)
 	fmt.Printf("%v", newArray2)
@@ -27,3 +29,17 @@ func ArrayLen(){
 
 	fmt.Println("Array lenght is ", lenght)
 }
+
+func MultiDimensional(){
+	var multiDimensionalArray [3][2]int = [3][2]int{[2]int{0,0}, [2]int{1,3}, [2]int{1,5}}
+	
+	//another way.. much better
+	var multiDimensionalArrayTwo [3][2]int
+	
+	multiDimensionalArrayTwo[0] = [2]int{0,0}
+	multiDimensionalArrayTwo[1] = [2]int{1,3}
+	multiDimensionalArrayTwo[2] = [2]int{1,5}
+
+	fmt.Println("Array lenght is ", multiDimensionalArray)
+}
+
